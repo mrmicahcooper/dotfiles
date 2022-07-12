e170a03f403bf8592ed9c2b7afa2c71a581d053a
Micah Cooper <micah+github@mrmicahcooper.com>
2022-07-12 11:48:01 -0400

    Style indent lines

--- .config/nvim/lua/options/init.lua
+++ .config/nvim/lua/options/init.lua
@@ -55,6 +55,8 @@ vim.api.nvim_set_keymap('n', '<c-B>',
 -- indent-blankline config
 -- vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
+-- ¦, ┆, │, ⎸, or ▏
+vim.g.indent_blankline_char = '┊'
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
