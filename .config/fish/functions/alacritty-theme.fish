bfd3859a12659a6e8ecbd1e2fb14de3376da3306
Micah Cooper <micah.cooper@salesforce.com>
2023-07-24 15:52:49 -0500

    Fix alacritty-theme function

--- .config/fish/functions/alacritty-theme.fish
+++ .config/fish/functions/alacritty-theme.fish
@@ -1,5 +1,5 @@
 function alacritty-theme --description "Pick a colorscheme"
-    set -l color (ls ~/.config/alacritty/themes/ | fzf)
+    set -l color (ls -1 ~/.config/alacritty/themes/ | fzf)
 
     cp ~/.config/alacritty/themes/$color \
        ~/.config/alacritty/current-theme.yml
