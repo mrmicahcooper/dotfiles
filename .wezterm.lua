b7051a42403353e9c01555bd05b26e2dceb2c9ff
Micah Cooper <micah.cooper@salesforce.com>
2023-12-12 11:20:02 -0600

    Intro wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require 'wezterm'
+return {
+  window_decorations = "RESIZE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+  },
+
+  -- color_scheme = "Catppuccin Latte", -- Mocha, Macchiato, Frappe, Latte
+  color_scheme = "Catppuccin Mocha", -- Mocha, Macchiato, Frappe, Latte
+  -- color_scheme = "Catppuccin Frappe", -- Mocha, Macchiato, Frappe, Latte
+  -- color_scheme = "Catppuccin Latte", -- Mocha, Macchiato, Frappe, Latte
+  --
+  -- font = wezterm.font 'Maple Mono NF',
+  font = wezterm.font '0xProto Nerd Font',
+  -- font = wezterm.font 'IntelOneMono Nerd Font',
+  -- font = wezterm.font 'FantasqueSansMono Nerd Font Mono',
+  -- font = wezterm.font 'JuliaMono',
+  -- font = wezterm.font 'VictorMono Nerd Font',
+  font_size = 15,
+  line_height = 1.1
+}
