86a078d744a11eb39aeb412c8a88ba39a2f670b7
Micah Cooper <micah+github@mrmicahcooper.com>
2023-12-16 15:36:43 -0600

    Update font dpi

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,16 +9,19 @@ return {
   },
 
   -- color_scheme = "Catppuccin Latte", -- Mocha, Macchiato, Frappe, Latte
-  color_scheme = "Catppuccin Mocha", -- Mocha, Macchiato, Frappe, Latte
+  -- color_scheme = "Catppuccin Mocha", -- Mocha, Macchiato, Frappe, Latte
   -- color_scheme = "Catppuccin Frappe", -- Mocha, Macchiato, Frappe, Latte
-  -- color_scheme = "Catppuccin Latte", -- Mocha, Macchiato, Frappe, Latte
+  color_scheme = "Catppuccin Macchiato", -- Mocha, Macchiato, Frappe, Latte
   --
-  -- font = wezterm.font 'Maple Mono NF',
-  font = wezterm.font '0xProto Nerd Font',
+  font = wezterm.font 'Maple Mono NF',
+  -- font = wezterm.font '0xProto Nerd Font',
   -- font = wezterm.font 'IntelOneMono Nerd Font',
   -- font = wezterm.font 'FantasqueSansMono Nerd Font Mono',
   -- font = wezterm.font 'JuliaMono',
   -- font = wezterm.font 'VictorMono Nerd Font',
-  font_size = 15,
-  line_height = 1.1
+  font_size = 20,
+  line_height = 1.1,
+  -- freetype_load_target = 'Light'
+  -- freetype_load_target = 'HorizontalLcd',
+  dpi = 96.0
 }
