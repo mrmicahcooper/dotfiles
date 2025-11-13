814aac6aa1bdd1ed538ee21ddb4786401692226b
Micah Cooper <micah+github@mrmicahcooper.com>
2025-11-12 20:57:06 -0600

    Update lua plugins

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -24,15 +24,15 @@ return {
     dependencies = { 'kyazdani42/nvim-web-devicons' }
   },
 
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   build = "cd app && yarn install",
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { "markdown" }
+  --   end,
+  --   ft = { "markdown" },
+  -- },
 
   {
     'numToStr/Comment.nvim',
