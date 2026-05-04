2e9a756ea7180ea8d3302ab3aee882df47f25ab7
Micah Cooper <micah.cooper@mimic.com>
2026-05-04 14:17:27 -0500

    Update neovim deps

--- private_dot_config/nvim/lua/config/plugins/init.lua
+++ private_dot_config/nvim/lua/config/plugins/init.lua
@@ -106,23 +106,23 @@ return {
     },
   },
 
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-    config = function()
-      require('render-markdown').setup({
-        code = {
-          conceal_delimiters = false
-        }
-      })
-    end
-  }, 
-
+  -- {
+  --   'MeanderingProgrammer/render-markdown.nvim',
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  --   ---@module 'render-markdown'
+  --   ---@type render.md.UserConfig
+  --   opts = {},
+  --   config = function()
+  --     require('render-markdown').setup({
+  --       code = {
+  --         conceal_delimiters = false
+  --       }
+  --     })
+  --   end
+  -- }, 
+  --
   {
     "folke/sidekick.nvim",
     opts = {
